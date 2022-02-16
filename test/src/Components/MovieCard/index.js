e76943d301e5e85dd1b5f58fdc2a583ba0b28e9b
Vincent Kouoï <vincentkouoi@gmail.com>
2022-02-16 11:23:25 +0100

    wip for tooltip

--- test/src/Components/MovieCard/index.js
+++ test/src/Components/MovieCard/index.js
@@ -1,22 +1,29 @@
-import React, { useState } from "react";
-import { Card } from "antd";
+import React from "react";
+import { Card, Tooltip } from "antd";
 import "./MovieCard.css";
 
 const MovieCard = (lol) => {
   const movieData = lol;
-
   return (
     <>
-      <Card
-        className="custom-card"
-        hoverable
-        cover={
-          <img
-            alt="#"
-            src={`https://image.tmdb.org/t/p/w500/${movieData.movieData.poster_path}`}
-          />
-        }
-      />
+      <Tooltip
+        title={movieData.movieData.overview}
+        placement="leftTop"
+        arrowPointAtCenter
+        color="black"
+        overlayInnerStyle={{ width: "650px", height: "100px" }}
+      >
+        <Card
+          className="custom-card"
+          hoverable
+          cover={
+            <img
+              alt="#"
+              src={`https://image.tmdb.org/t/p/w500/${movieData.movieData.poster_path}`}
+            />
+          }
+        />
+      </Tooltip>
     </>
   );
 };
